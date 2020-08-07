c8d0cd24a3bdfd2528661707d974fb6d86de9016
Gabriel Tagliari Rodrigo <gtagliari@ciandt.com>
2020-08-07 13:43:41 -0300

    Initial commit of simple structure

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import App from './app';
+
+const PORT = process.env.SERVER_PORT || 8000;
+
+App.app.get('/', (req, res) => res.send('Express + TypeScript Server is cool!'));
+
+App.app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+module.exports = App.app;
